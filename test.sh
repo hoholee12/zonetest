6d2bff04fd92c16e78b5cb55a721e807c129648a
hoholee12 <hoholee12@gmail.com>
2022-04-07 15:25:58 +0900

    a

--- test.sh
+++ test.sh
@@ -1,16 +1,24 @@
-#!/bin/bash
++#!/bin/bash
 
+#2mb
 ./sata_f2fs_custom.sh 1 1
 #./fragment.sh
-iozone -R -b bench0.xls -i 0 -n 512m -g 10g -f /home/jeongho/mnt/fiotest
+iozone -R -b bench0.xls -i 0 -i 1 -s 10G -f /home/jeongho/mnt/iozonetest
 
+#200mb
 ./sata_f2fs_custom.sh 1 100
 #./fragment.sh
-iozone -R -b bench1.xls -i 0 -n 512m -g 10g -f /home/jeongho/mnt/fiotest
+iozone -R -b bench1.xls -i 0 -i 1 -s 10G -f /home/jeongho/mnt/iozonetest
 
+#2gb
+./sata_f2fs_custom.sh 1 1000
+#./fragment.sh
+iozone -R -b bench2.xls -i 0 -i 1 -s 10G -f /home/jeongho/mnt/iozonetest
+
+#20gb
 ./sata_f2fs_custom.sh 1 10000
 #./fragment.sh
-iozone -R -b bench2.xls -i 0 -n 512m -g 10g -f /home/jeongho/mnt/fiotest
+iozone -R -b bench2.xls -i 0 -i 1 -s 10G -f /home/jeongho/mnt/iozonetest
 
-#iozone -R -b reportfile -i testid -n minfilesize -g maxfilesize -r blocksize -a blocksizeincreases -l lowerthread# -u upperthread# -f <filelocation>
+#iozone -R -b reportfile -i testid -I directio -s filesize -r blocksize -a blocksizeincreases -l lowerthread# -u upperthread# -f <filelocation>
 #testid: 0: write/rewrite, 1: read/reread, 2: randread/randwrite
