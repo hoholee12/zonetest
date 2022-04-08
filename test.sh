23d4b7a15a221a1f600d80adfa1cab25709eae93
hoholee12 <hoholee12@gmail.com>
2022-04-08 15:54:34 +0900

    results

--- test.sh
+++ test.sh
@@ -2,31 +2,36 @@
 
 #2mb
 ./sata_f2fs_custom.sh 1 1
-./fragment.sh
+#./fragment.sh
+./justfill.sh
 #iozone -R -b bench0.xls -i 0 -i 1 -s 10G -f /home/jeongho/mnt/iozonetest
 ./testfio.sh zone2mb
 
 #20mb
 ./sata_f2fs_custom.sh 1 10
-./fragment.sh
+#./fragment.sh
+./justfill.sh
 #iozone -R -b bench0.xls -i 0 -i 1 -s 10G -f /home/jeongho/mnt/iozonetest
 ./testfio.sh zone20mb
 
 #200mb
 ./sata_f2fs_custom.sh 1 100
-./fragment.sh
+#./fragment.sh
+./justfill.sh
 #iozone -R -b bench1.xls -i 0 -i 1 -s 10G -f /home/jeongho/mnt/iozonetest
 ./testfio.sh zone200mb
 
 #2gb
 ./sata_f2fs_custom.sh 1 1000
-./fragment.sh
+#./fragment.sh
+./justfill.sh
 #iozone -R -b bench2.xls -i 0 -i 1 -s 10G -f /home/jeongho/mnt/iozonetest
 ./testfio.sh zone2gb
 
 #20gb
 ./sata_f2fs_custom.sh 1 10000
-./fragment.sh
+#./fragment.sh
+./justfill.sh
 #iozone -R -b bench2.xls -i 0 -i 1 -s 10G -f /home/jeongho/mnt/iozonetest
 ./testfio.sh zone20gb
 
