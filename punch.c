148b1038ca7dea597cb6bb286430e9a5a130c3e4
hoholee12 <hoholee12@gmail.com>
2022-04-10 06:05:55 -0700

    Edit punch

--- punch.c
+++ punch.c
@@ -6,21 +6,28 @@
 
 int main(int argc, char* argv[]){
 	long long x = 250966470656;
-	if(argc < 2){ printf("i need file to work with\n"); return 0; }
+	int blocksize = 4096;
+	if(argc < 2){ printf("i need file to work with\n"); exit(1);}
 	int fd;
 	printf("punching %s...\n", argv[1]);
-	if(argc == 3){ x=atoi(argv[2])*1048576; printf("size=%lld bytes\n", x);}
+	fd = open(argv[1], O_RDWR|O_LARGEFILE);
+	if(fd == -1){ printf("file doesnt exist?\n"); exit(1);}
+	x = lseek64(fd, 0, SEEK_END);
+	close(fd);
+	printf("filesize = %lld\n", x);
+	if(argc == 3) blocksize = atoi(argv[2]);
+	printf("blocksize = %d\n", blocksize);
 	for(long long j = 3; j <= 10; j+=7){
-		fd = open(argv[1], O_RDWR);
-		if(fd == -1){ printf("file doesnt exist?\n"); return 0;}
-		for(long long i = 0; i < x; i += 4096*j){
+		fd = open(argv[1], O_RDWR|O_LARGEFILE);
+		if(fd == -1){ printf("file doesnt exist?\n"); exit(1);}
+		for(long long i = 0; i < x; i += blocksize*j){
 			printf("\r%lld percent...  %lld", 100/j, i);
-			fallocate(fd, FALLOC_FL_PUNCH_HOLE|FALLOC_FL_KEEP_SIZE, i, 4096);
+			fallocate(fd, FALLOC_FL_PUNCH_HOLE|FALLOC_FL_KEEP_SIZE, i, blocksize);
 		}
 		printf("\n");
 		close(fd);
 	}
 	printf("done\n");
-	
+
 	return 0;
 }
